4cf6a2687941022f2931caaf10692e9fc80fbe6e
SimonGrFo <simgrafor03@gmail.com>
2023-09-25 23:22:24 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner USERNAME = new Scanner(System.in);
+        System.out.println("Vad heter du?: ");
+        System.out.println("Du heter: " + USERNAME.next());
+    }
+
+}
